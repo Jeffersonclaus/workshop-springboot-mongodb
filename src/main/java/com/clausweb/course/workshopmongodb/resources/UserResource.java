78fa532d1de5b050541f572d823019efc633704c
Jefferson Claus <jeffersonclaus@gmail.com>
2021-09-28 16:21:17 -0300

    atualizacao put com o metodo update

--- src/main/java/com/clausweb/course/workshopmongodb/resources/UserResource.java
+++ src/main/java/com/clausweb/course/workshopmongodb/resources/UserResource.java
@@ -25,6 +25,8 @@ public class UserResource {
 	@Autowired
 	UserService service;
 
+	
+	//findAll()
 	@RequestMapping(method = RequestMethod.GET)
 	public ResponseEntity<List<UserDTO>> Findall() {
 
@@ -35,32 +37,38 @@ public class UserResource {
 
 	}
 
-
-	
-	
-	@PostMapping
-	public ResponseEntity<Void>Insert(@RequestBody UserDTO objDTO){
-		User obj = service.fromDTO(objDTO);
-		
-		obj = service.insert(obj);
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").
-				buildAndExpand(obj.getId()).toUri();  
-		return ResponseEntity.created(uri).build();  
+	//findById()
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(new UserDTO(obj));
 	}
-	
-	
-	
-	@RequestMapping(value = "/{id}" ,method = RequestMethod.DELETE)
-	public ResponseEntity<Void> delete(@PathVariable String id){
-		
-		
+
+	//Insert()
+	@PostMapping
+	public ResponseEntity<Void> Insert(@RequestBody UserDTO objDTO) {
+		User obj = service.fromDTO(objDTO);
+
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+
+	//delete()
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable String id) {
+
 		service.delete(id);
 		return ResponseEntity.noContent().build();
-		
-		
-		
-	}
-	 
 
-	
+	}
+
+	//update()
+	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
+ 	public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id) {
+		User obj = service.fromDTO(objDto);
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+	}
 }
